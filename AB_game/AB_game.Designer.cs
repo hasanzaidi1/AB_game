70036a357144c01967a0255ce7c830fd25b49c52
Administrator <Administrator@EC2AMAZ-F2U0CVB>
2024-04-15 19:24:17 +0000

    updated ab_game.cs

--- AB_game/AB_game.Designer.cs
+++ AB_game/AB_game.Designer.cs
@@ -38,36 +38,42 @@
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(368, 221);
+            textBox1.Location = new Point(258, 133);
+            textBox1.Margin = new Padding(2);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(150, 31);
+            textBox1.Size = new Size(106, 23);
             textBox1.TabIndex = 0;
             // 
             // Submitbtn
             // 
-            Submitbtn.Location = new Point(541, 221);
+            Submitbtn.Location = new Point(379, 133);
+            Submitbtn.Margin = new Padding(2);
             Submitbtn.Name = "Submitbtn";
-            Submitbtn.Size = new Size(112, 34);
+            Submitbtn.Size = new Size(78, 20);
             Submitbtn.TabIndex = 1;
             Submitbtn.Text = "Submit";
             Submitbtn.UseVisualStyleBackColor = true;
+            Submitbtn.Click += Submitbtn_Click;
             // 
             // HistoryBox
             // 
             HistoryBox.FormattingEnabled = true;
-            HistoryBox.ItemHeight = 25;
-            HistoryBox.Location = new Point(709, 62);
+            HistoryBox.ItemHeight = 15;
+            HistoryBox.Location = new Point(496, 37);
+            HistoryBox.Margin = new Padding(2);
             HistoryBox.Name = "HistoryBox";
-            HistoryBox.Size = new Size(180, 204);
+            HistoryBox.Size = new Size(127, 124);
             HistoryBox.TabIndex = 2;
+            HistoryBox.SelectedIndexChanged += HistoryBox_SelectedIndexChanged;
             // 
             // GuessLbl
             // 
             GuessLbl.AutoSize = true;
             GuessLbl.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
-            GuessLbl.Location = new Point(326, 129);
+            GuessLbl.Location = new Point(228, 77);
+            GuessLbl.Margin = new Padding(2, 0, 2, 0);
             GuessLbl.Name = "GuessLbl";
-            GuessLbl.Size = new Size(244, 32);
+            GuessLbl.Size = new Size(162, 21);
             GuessLbl.TabIndex = 3;
             GuessLbl.Text = "Codebreakers Guess";
             // 
@@ -75,9 +81,10 @@
             // 
             Attempts.AutoSize = true;
             Attempts.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
-            Attempts.Location = new Point(626, 312);
+            Attempts.Location = new Point(438, 187);
+            Attempts.Margin = new Padding(2, 0, 2, 0);
             Attempts.Name = "Attempts";
-            Attempts.Size = new Size(148, 28);
+            Attempts.Size = new Size(103, 19);
             Attempts.TabIndex = 4;
             Attempts.Text = "Attempts Left:";
             // 
@@ -85,24 +92,27 @@
             // 
             AttemptsLeft.AutoSize = true;
             AttemptsLeft.Font = new Font("Segoe UI", 10F);
-            AttemptsLeft.Location = new Point(780, 312);
+            AttemptsLeft.Location = new Point(546, 187);
+            AttemptsLeft.Margin = new Padding(2, 0, 2, 0);
             AttemptsLeft.Name = "AttemptsLeft";
-            AttemptsLeft.Size = new Size(20, 28);
+            AttemptsLeft.Size = new Size(15, 19);
             AttemptsLeft.TabIndex = 5;
             AttemptsLeft.Text = "-";
+            AttemptsLeft.Click += AttemptsLeft_Click;
             // 
-            // ABgame
+            // AB_game
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(924, 450);
+            ClientSize = new Size(647, 270);
             Controls.Add(AttemptsLeft);
             Controls.Add(Attempts);
             Controls.Add(GuessLbl);
             Controls.Add(HistoryBox);
             Controls.Add(Submitbtn);
             Controls.Add(textBox1);
-            Name = "ABgame";
+            Margin = new Padding(2);
+            Name = "AB_game";
             Text = "Form1";
             ResumeLayout(false);
             PerformLayout();
--- AB_game/AB_game.cs
+++ AB_game/AB_game.cs
@@ -2,11 +2,45 @@ namespace AB_game
 {
     public partial class AB_game : Form
     {
+
+        public int currAttempts = 10;
+        public string enteredNum;
+        List<string> enteredNumList = new List<string>();
+
         public AB_game()
         {
             InitializeComponent();
         }
 
-        
+        private void AttemptsLeft_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Submitbtn_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == string.Empty)
+            {
+                MessageBox.Show("Enter a numerical value");
+            }
+            else
+            {
+                enteredNum = (textBox1.Text).ToString();
+                enteredNumList.Add(enteredNum);
+                currAttempts -= 1;
+            }
+            AttemptsLeft.Text = currAttempts.ToString();
+
+            if (currAttempts == 0) { Submitbtn.Enabled = false; }
+
+            textBox1.Text = string.Empty;
+            string joinedString = string.Join(Environment.NewLine, enteredNumList);
+            HistoryBox.Text = joinedString;
+        }
+
+        private void HistoryBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
