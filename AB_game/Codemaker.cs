f649497cc1d25f38e668da629461cbd1dd8af411
hasanzaidi1 <106449458+hasanzaidi1@users.noreply.github.com>
2024-04-19 22:51:13 -0400

    Generates the code and matches it correctly also HASAN

--- AB_game/Codemaker.cs
+++ AB_game/Codemaker.cs
@@ -1,56 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
 namespace AB_game
 {
     public partial class Codemaker : Form
     {
+        public string secretNumber;
 
-        public int currAttempts = 10;
-        //List<string> enteredNumList;
-        public string secretNumber; 
-
-        WelcomeForm welcomeForm = new WelcomeForm();
-
-
-        public Codemaker()
+        public Codemaker(string secretNum)
         {
-            secretNumber = GenerateSecretNumber();
             InitializeComponent();
-        }
-
-        // Generate a random four-digit number with no repeating digits
-        public string GenerateSecretNumber()
-        {
-            Random random = new Random();
-            List<int> digits = new List<int>();
-
-            while (digits.Count < 4)
-            {
-                int num = random.Next(0, 10); // Generate a number between 0 and 9
-                if (!digits.Contains(num))    // Ensure there are no duplicates
-                {
-                    digits.Add(num);
-                }
-            }
-
-            return string.Join("", digits);  // Convert List<int> to string
+            this.secretNumber = secretNum;
         }
 
         private void GenerateNum_Click(object sender, EventArgs e)
-        {
+        {            
             Num.Text = secretNumber.ToString();
-            GenerateNum.Enabled = false;
         }
 
-        public void CompareNum(string guessedNum)
-        {
-            if(secretNumber.Equals(guessedNum))
-            {
-                MessageBox.Show("Correct");
-            } 
-            else
-            {
-                MessageBox.Show("Not Correct ");
-            }
-        }
+
 
     }
 }
